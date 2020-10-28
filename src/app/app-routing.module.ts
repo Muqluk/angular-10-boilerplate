e372dce3b50f2aaf9e937b49d4723ff9766c0a0c
Jeremy Little <muqluk@yahoo.com>
2020-10-27 20:05:17 -0500

    css updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,29 +20,10 @@ const routes = [ // eslint-disasble-line
     // loadChildren: 'modules/ghp-common/ghp-common.module#GhpCommonModule',
     icon: 'k-i-images'
   },
-  // {
-  //   path: 'mod-a',
-  //   text: 'Module A',
-  //   loadChildren: () => import('modules/module-a/mod-a.module').then(m => m.ModAModule),
-  //   icon: 'k-i-form k-i-border'
-  // },
-  // {
-  //   path: 'mod-b',
-  //   text: 'Module B',
-  //   loadChildren: () => import('modules/module-b/mod-b.module').then(m => m.ModBModule),
-  //   icon: 'k-i-textbox-hidden'
-  // },
-  // {
-  //   path: 'mod-c',
-  //   text: 'Module C',
-  //   loadChildren: () => import('modules/module-c/mod-c.module').then(m => m.ModCModule),
-  //   icon: 'k-i-module-manager'
-  // },
 ];
 
 @NgModule({
   imports: [
-    // RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading })
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
