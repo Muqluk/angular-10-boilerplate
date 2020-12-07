633fbe2c68b780656b95ee43d9215de9c398c65e
Jeremy Little <jwlittle@maxor.com>
2020-12-07 04:28:15 -0600

    saving work from a few months back

--- src/modules/ghp-common/components/grid/grid.ts
+++ src/modules/ghp-common/components/grid/grid.ts
@@ -1,16 +1,27 @@
+/* eslint-disable */
+
 import {
   AfterViewInit,
   Component,
   ElementRef,
   EventEmitter,
+  HostBinding,
   Input,
   OnInit,
   Output,
   ViewChild
 } from "@angular/core";
 
-import { ColumnComponent, GridComponent } from "@progress/kendo-angular-grid";
-import { State, DataResult } from "@progress/kendo-data-query";
+import {
+  ColumnComponent,
+  GridComponent,
+  ScrollMode
+} from "@progress/kendo-angular-grid";
+
+import {
+  State,
+  DataResult
+} from "@progress/kendo-data-query";
 
 interface ColumnSettings {
   field: string;
@@ -35,46 +46,25 @@ interface GridSettings {
   styleUrls: ['grid.scss'],
   templateUrl: 'grid.html',
 })
-class Grid implements AfterViewInit, OnInit {
-  @ViewChild("gridRef") gridRef: GridComponent;
-  //#region Data and Behaviors
+class Grid {
+  @HostBinding('class.g-grid-component') useClass: boolean = true;
+
+  @ViewChild("gridRef") set _gridRef(ref: GridComponent) {
+    console.log(ref);
+  }
   @Output() selections = new EventEmitter();
 
-  @Input() data: Array<any> = []; // eslint-disable-line
+  @Input() data: Array<any> = [];
+  @Input() columns: Array<any> = [];
   @Input() loading: boolean = false;
-  @Input() pageable: boolean = true;
-  @Input() pageSize: number = 250;
+  @Input() pageable: boolean = false;
+  @Input() pageSize: number = 50;
+  @Input() height: number = 250;
+  @Input() scrollable: ScrollMode = 'scrollable';
 
-  //#endregion
-
-  //#region Something
-
-  @Input() height: number = 50;
-
-  //#endregion
-
-  //#region Events
-
-
-  columnVisibilityChange(cols: any): void {// eslint-disable-line
-    console.log(cols); // eslint-disable-line
-  }
-
-
-  //#endregion
-
-  ngOnInit(): void {
-    // nothing
-  }
-
-  ngAfterViewInit(): void {
-    // const cols = this.gridRef.columns;
-    // console.log("gridRef.columns:", cols.toArray()); // eslint-disable-line
-  }
-
-  clikity(grid: GridComponent): void { 
-    const cols = grid.columns.toArray();
-    console.log("gridRef.columns:", cols); // eslint-disable-line
+  onColumnResize(evt: any) {
+    const { column, newWidth } = evt[0];
+    console.log(`${column.title}: ${newWidth}`);
   }
 }
 
--- src/modules/ghp-common/pages/gallery/tabs/component-sandbox/component-sandbox.ts
+++ src/modules/ghp-common/pages/gallery/tabs/component-sandbox/component-sandbox.ts
@@ -9,6 +9,56 @@ import {
   TProductResult,
 } from "./models";
 
+import { products } from "./products";
+
+const getProducts = () => products.rows.map((p: any): any => {
+  const { ProductId,
+    Title,
+    Price,
+    PublishingCategory,
+    LicenseCatgory,
+    Series,
+    Author,
+    PubDate,
+    Media,
+    ISBN,
+    ShortTitle,
+    Publisher,
+    Copyright, } = p;
+  return {
+    ProductId,
+    Title,
+    Price,
+    PublishingCategory,
+    LicenseCatgory,
+    Series,
+    Author,
+    PubDate,
+    Media,
+    ISBN,
+    ShortTitle,
+    Publisher,
+    Copyright,
+  };
+});
+
+/*
+  { 
+    ProductId
+    Title
+    Price
+    PublishingCategory
+    LicenseCatgory
+    Series
+    Author
+    PubDate
+    Media
+    ISBN
+    ShortTitle
+    Publisher
+    Copyright
+  },
+*/
 
 @Component({
   selector: 'component-sandbox',
@@ -17,29 +67,27 @@ import {
 })
 export class ComponentSandbox implements OnInit {
   ProductsResult: TProductResult = new TProductResult();
-  Products: Array<TProduct> = [];
+  Products: Array<any> = [];
   loading: boolean = false;
+  columns = [
+    { field: 'ProductId', title: 'ProductId', width: 85 },
+    { field: 'Author', title: 'Author', width: 200 },
+    { field: 'Title', title: 'Title', width: 300 },
+    { field: 'ShortTitle', title: 'ShortTitle', width: 200 },
+    { field: 'Series', title: 'Series', width: 200 },
+    { field: 'Media', title: 'Media', width: 100 },
+    // { field: 'Price', title: 'Price', width: 100, hidden: true},
+    // { field: 'PublishingCategory', title: 'PublishingCategory', width: 100, hidden: true },
+    // { field: 'LicenseCatgory', title: 'LicenseCatgory', width: 100, hidden: true },
+    // { field: 'PubDate', title: 'PubDate', width: 100, hidden: true },
+    // { field: 'ISBN', title: 'ISBN', width: 100, hidden: true },
+    { field: 'Publisher', title: 'Publisher', width: 100 },
+    // { field: 'Copyright', title: 'Copyright', width: 100, hidden: true },
+  ];
 
   constructor(private api: RestProvider) { }
 
   ngOnInit() {
-    this.bindFunctions();
-    this.loading = true;
-    this.api
-      .get('Products')
-      .then(this.setProducts)
-      .then(() => {
-        this.loading = false;
-      });
-  }
-
-  bindFunctions() {
-    this.setProducts = this.setProducts.bind(this);
-  }
-
-  setProducts(products: TProductResult) {
-    // this.ProductsResult = products;
-    console.log(products.rows);
-    this.Products = products.rows.slice(1, 1000);
+    this.Products = getProducts();
   }
 }
